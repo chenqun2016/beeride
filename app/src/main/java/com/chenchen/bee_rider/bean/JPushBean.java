f791d4fea6251317526085b31613bbbc92940720
bixueyun <506154588@qq.com>
2022-03-05 15:28:43 +0800

    极光推送接入一

---
+++ app/src/main/java/com/chenchen/bee_rider/bean/JPushBean.java
@@ -0,0 +1,10 @@
+package com.chenchen.bee_rider.bean;
+
+/**
+ - @Description:
+ - @Author: bxy
+ - @Time:  2021/9/12 下午2:48
+ */
+public class JPushBean {
+
+}
---
+++ app/src/main/java/com/chenchen/bee_rider/jpush/PushService.java
@@ -0,0 +1,8 @@
+package com.chenchen.bee_rider.jpush;
+
+
+import cn.jpush.android.service.JCommonService;
+
+public class PushService extends JCommonService {
+
+}
